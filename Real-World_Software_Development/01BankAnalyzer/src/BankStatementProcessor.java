f49498d55a22c23ca1475414e7c3a57d53cf7e38
gangfunction <gangfunction@gmail.com>
2022-11-23 16:10:20 +0900

    updated

---
+++ Real-World_Software_Development/01BankAnalyzer/src/BankStatementProcessor.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class BankStatementProcessor {
+    private final List<BankTransaction> bankTransaction;
+    public BankStatementProcessor(final List<BankTransaction> bankTransactions)
+    {
+        this.bankTransaction = bankTransactions;
+
+    }
+    public double calculateTotalAmount()
+}
